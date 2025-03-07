9edd201325ff159cf8dec899a5fbfd8bd716fcaa
Ersin Mert Demircan <e.mertdemircan@gmail.com>
2025-03-07 14:44:15 +0300

    calculation entity model edited

--- DisabilityCompensation.Domain/Entities/CompensationCalculation.cs
+++ DisabilityCompensation.Domain/Entities/CompensationCalculation.cs
@@ -10,17 +10,17 @@ namespace DisabilityCompensation.Domain.Entities
         public Guid CompensationId { get; set; }
         public DateOnly StartDate { get; set; }
         public DateOnly EndDate { get; set; }
+        public decimal Wage { get; set; }
+        public int TotalDays { get; set; }
         public decimal DisabilityRate { get; set; }
         public decimal FaultRate { get; set; }
         public decimal Amount { get; set; }
         public string? Gender { get; set; }
+        public string? MaritalStatus { get; set; }
         public int Age { get; set; }
-        public int TotalDays { get; set; }
-        public decimal Wage { get; set; }
         public int? NumberOfChilren { get; set; }
         public Periods Period { get; set; }
         public SalaryCalculatorTypes CalculatorType { get; set; }
-        public string? MaritalStatus { get; set; }
 
         #region Relations
 
